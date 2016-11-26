20f6526f88a4c4739745ba611e59a32c4490ea17
PLK <PLK@172.17.9.66>
2016-11-26 16:46:49 -0500

    Eager loading dao film OK

--- src/locafilm/DAOFilm.java
+++ src/locafilm/DAOFilm.java
@@ -5,9 +5,11 @@
  */
 package locafilm;
 import java.util.*;
+import org.hibernate.FetchMode;
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.criterion.*;
 /**
  *
  * @author PLK
@@ -26,5 +28,13 @@ public class DAOFilm {
         factory.close();
 
         return films;
+    }
+    
+    public static Film getFilmInfos(int id){
+        final SessionFactory factory = HibernateUtil.getSessionFactory();
+        final Session s = factory.openSession();
+        List<Film> result = s.createCriteria(Film.class).add(Restrictions.eq("IDFILM",id)).list();
+        
+        return result.get(0);
     }
 }
